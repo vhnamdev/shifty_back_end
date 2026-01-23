ca5d08b39ffd6fe19a34c472a1ed28a0c44ed52f
NoCodeNoLife-Dev <nocodenolife.official@gmail.com>
2026-01-24 00:25:55 +0700

    create middleware, jwt

--- internal/delivery/http/route/router.go
+++ internal/delivery/http/route/router.go
@@ -1,14 +1,17 @@
 package route
 
 import (
+	"shifty-backend/internal/delivery/http/middleware"
+	"shifty-backend/pkg/token"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 type AppHandlers struct {
 }
 
-func SetupRoutes(app *fiber.App, handlers *AppHandlers) {
-	api := app.Group("/api/v1")
+func SetupRoutes(app *fiber.App, handlers *AppHandlers, tokenMaster *token.TokenMaster) {
+	api := app.Group("/api/v1", middleware.Protected(tokenMaster))
 	api.Get("/health", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{
 			"status":  "success",
